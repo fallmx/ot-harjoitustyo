5c108f0da374b125136e0cbe1af3916a42913f29
fallmx <31561277+fallmx@users.noreply.github.com>
2024-04-30 22:42:09 +0300

    Add basic test for AudioPlayer

---
+++ src/tests/test_audio_player.py
@@ -0,0 +1,15 @@
+# SPDX-License-Identifier: GPL-3.0-or-later
+import unittest
+from audio_player import AudioPlayer
+
+TEST_FILE = "src/tests/test_data/test.wav"
+TEST_FILE_SAMPLES = 160195
+
+class TestAudioPlayer(unittest.TestCase):
+    def setUp(self):
+        self.player = AudioPlayer()
+    
+    def test_load_works(self):
+        self.player.load_file(TEST_FILE)
+        sample_amount = len(self.player._data)
+        self.assertEqual(sample_amount, TEST_FILE_SAMPLES)
